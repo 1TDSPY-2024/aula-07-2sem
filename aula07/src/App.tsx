39fab8e1ee6a21491a3617cd669132d350593a23
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-27 19:51:55 -0300

    Criando o outlet e alterando os títulos das rotas.

--- aula07/src/App.tsx
+++ aula07/src/App.tsx
@@ -1,7 +1,9 @@
+import { Outlet } from "react-router-dom";
+
 export default function App(){
   return(
     <div>
-      <h1>Olá, mundo sou o APP!</h1>
+      <Outlet/>
     </div>
   );
 }
--- aula07/src/routes/Error/index.tsx
+++ aula07/src/routes/Error/index.tsx
@@ -1,4 +1,8 @@
 export default function Error(){
+
+      //MUDANDO O TÍTULO DA PÁGINA!!!
+      document.title = "Erro 404";
+
     return(
       <div>
         <h1>Olá, mundo sou o Error!</h1>
