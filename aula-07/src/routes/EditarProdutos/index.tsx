6f3116dfc8d5e6bb88c38c56302d46425dfca806
vitorvhsilva <dndragonbr@gmail.com>
2024-08-27 20:58:25 -0300

    useParams vindo da url

--- aula-07/src/routes/EditarProdutos/index.tsx
+++ aula-07/src/routes/EditarProdutos/index.tsx
@@ -1,7 +1,13 @@
+import { useParams } from "react-router-dom"
+
 export default function EditarProdutos() {
+
+  const { id } = useParams()
+
   return (
     <div>
       <h1>Olá mundo, sou o EditarProdutos</h1>
+      <p>{id}</p>
     </div>
   )
 }
