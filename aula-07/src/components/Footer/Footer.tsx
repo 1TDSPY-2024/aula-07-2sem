daa95d52e878240ec969009b76b5806ba4a8e340
vitorvhsilva <dndragonbr@gmail.com>
2024-08-27 20:33:25 -0300

    Finalização dos componentes das rotas

---
+++ aula-07/src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <h3>Rodape</h3>
+    </footer>
+  )
+}
---
+++ aula-07/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import Menu from "../Menu/Menu";
+
+export default function Header() {
+  return (
+    <header>
+      <h3>Cabecalho</h3>
+      <Menu/>
+    </header>
+  )
+}
