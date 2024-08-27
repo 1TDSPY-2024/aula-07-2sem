00348eea7fd9f4cbe858257bdc525666d0b2d114
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2024-08-27 19:25:16 -0300

    Router-Dom Instalado e componentes de rotas importados no main.tsx

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,7 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
 import App from './App.tsx'
-import './index.css'
+import Home from './routes/Home/index.tsx'
+import EditarProdutos from './routes/EditarProdutos/index.tsx'
+import Produtos from './routes/Produtos/index.tsx'
+import Error from './routes/Error/index.tsx'
+
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
